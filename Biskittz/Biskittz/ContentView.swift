c9d9196de8c903c6522c9738a34fbd5621a89f5a
Salwa Aisyah <2110512052@mahasiswa.upnvj.ac.id>
2026-06-12 14:41:42 +0700

    chore: init Biskittz Xcode project at repo root

---
+++ Biskittz/Biskittz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Biskittz
+//
+//  Created by Salwa Aisyah Adhani on 12/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
