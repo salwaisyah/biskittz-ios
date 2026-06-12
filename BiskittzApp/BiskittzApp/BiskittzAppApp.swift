5ae7e149c4c454c59fbaf1b5928168bcc4d08fca
Salwa Aisyah <2110512052@mahasiswa.upnvj.ac.id>
2026-06-12 14:29:31 +0700

    chore: init Xcode project with MVVM folder structure

---
+++ BiskittzApp/BiskittzApp/BiskittzAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiskittzAppApp.swift
+//  BiskittzApp
+//
+//  Created by Salwa Aisyah Adhani on 12/06/26.
+//
+
+import SwiftUI
+
+@main
+struct BiskittzAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
