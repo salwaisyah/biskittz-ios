094a41add84b0d71985b62317d175da0c6cbe207
Salwa Aisyah <2110512052@mahasiswa.upnvj.ac.id>
2026-06-13 20:46:43 +0700

    feat: add activity card component

---
+++ Biskittz/Views/Components/ActivityCardView.swift
@@ -0,0 +1,56 @@
+//
+//  ActivityCardView.swift
+//  Biskittz
+//
+//  Created by Salwa Aisyah Adhani on 13/06/26.
+//
+
+import SwiftUI
+
+struct ActivityCardView: View {
+    let item: ActivityModel
+    
+    var body: some View {
+        VStack (alignment: .leading, spacing: 12){
+            Text(item.title)
+                .font(.headline)
+                .lineLimit(2)
+            
+            HStack {
+                Text(item.formattedDuration)
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.blue)
+                    
+                Text("\(item.sessionCount) sessions completed")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 12)
+                .fill(Color.gray.opacity(0.08))
+                .shadow(color: Color.black.opacity(0.4), radius: 8, x: 0, y: 4)
+        )
+        .glassEffect(.clear, in: .rect(cornerRadius:12))
+    }
+}
+
+#Preview {
+
+    // Create an activity id first to feed both Activity and Sessions
+    let activityId = UUID()
+    let activity = ActivityModel(
+        id: activityId,
+        title: "Study SwiftUI: State & Binding",
+        duration: 300,
+        sessions: [
+            SessionModel(activityId: activityId, duration: 5),
+            SessionModel(activityId: activityId, duration: 5),
+            SessionModel(activityId: activityId, duration: 5)
+        ]
+    )
+    return ActivityCardView(item: activity)
+}
