8894a90aba25771898173a8b9876b865f20b2040
Salwa Aisyah <2110512052@mahasiswa.upnvj.ac.id>
2026-06-16 20:06:30 +0700

    refactor: remove padding from the sheet

--- Biskittz/Views/Screens/Home/HomeScreenView.swift
+++ Biskittz/Views/Screens/Home/HomeScreenView.swift
@@ -76,7 +76,6 @@ struct HomeScreenView: View {
                             isShowingSheet: $isShowingSheet,
                             activityViewModel: activityViewModel
                         )
-                        .padding()
                         .presentationDetents([.large])
                     }
                 }
